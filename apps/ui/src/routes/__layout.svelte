5b62a8a96d8452716f2e9c702c9f924b03b35a9e
hyddeos <ericsageby@hotmail.com>
2022-12-19 20:49:44 +0100

    fix the icon size of the logout icon

--- apps/ui/src/routes/__layout.svelte
+++ apps/ui/src/routes/__layout.svelte
@@ -329,7 +329,7 @@
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
-								class="ml-1 h-8 w-8"
+								class="ml-1 h-9 w-9"
 								viewBox="0 0 24 24"
 								stroke-width="1.5"
 								stroke="currentColor"
